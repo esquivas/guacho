ccb2c4c91ef7fa30cdc5c920817b5a70e515e1a1
~/.gitconfig <esquivel@nemesis.nucleares.unam.mx>
2016-03-10 12:02:08 -0600

    Clean documentation

--- doc/html/search/variables_b.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['max_5fiter',['max_iter',['../namespacethermal__cond.html#a5420f28be46174eb6852f14281ec7ace',1,'thermal_cond']]],
-  ['mjup',['mjup',['../namespaceconstants.html#a399e5e2bceae23d80dfe33489603dfa5',1,'constants']]],
-  ['msun',['msun',['../namespaceconstants.html#a47dcdcdf147127ab93252565436ce046',1,'constants']]],
-  ['myr',['myr',['../namespaceconstants.html#af554c9cfbc1f3c74a8176265ea5370dd',1,'constants']]]
-];
--- doc/html/search/variables_f.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['rank',['rank',['../namespaceglobals.html#a93f283242b146b3ef8b53fd1b1ad7bac',1,'globals']]],
-  ['rg',['rg',['../namespaceconstants.html#aab4c0a2b0e8b8cda79e9d683b3e650f6',1,'constants']]],
-  ['right',['right',['../namespaceglobals.html#a0b27e5c90473860434fc5dd58a255111',1,'globals']]],
-  ['rjup',['rjup',['../namespaceconstants.html#a4cbd85fb12d8faa8be8463982fa6fd29',1,'constants']]],
-  ['rsun',['rsun',['../namespaceconstants.html#a86e80095a8a44315466905ab42ec2814',1,'constants']]]
-];
