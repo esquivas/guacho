c2bba49d7ae42abd7b7acba29d49cc46baf658be
~/.gitconfig <esquivel@nemesis.nucleares.unam.mx>
2016-04-13 18:17:26 -0500

    return primitives (in cgs) instead of scaled Us

--- py/guacho_utils.py
+++ py/guacho_utils.py
@@ -33,6 +33,36 @@ def read_header(file_in, verbose=True):
   return (f,f_kind,(nx,ny,nz),(dx,dy,dz),(x0,y0,z0),\
          (mpi_x,mpi_y,mpi_z),neqs,neqdyn,nghost,(rsc,vsc,rhosc),cv)
 
+  '''
+    converts conserved variables to primitives (and de-scales to cgs)
+  '''
+def u2prim(file_in, ublock, equation, mhd = False) :
+  head_info = read_header(file_in,verbose=False)
+  rsc, vsc, rhosc = head_info[9]
+  cv              = head_info[10]
+  pblock = ublock
+  if equation == 0 :
+    #  density
+    pblock[0,::] = pblock[0,::] * rhosc
+  elif (equation >=1 and equation <= 3) :
+    #  velocity components
+    pblock[equation,::] = pblock[equation,::] / pblock[0,::] * vsc
+  elif equation == 4 :
+    #  thermal pressure
+    pblock[1:4,::] = ublock[1:4,::] / ublock[0,::]
+    pblock[4,::]   = ( ublock[4,::] - 0.5 * pblock[0,::]*( pblock[1,::]**2
+                                                       + pblock[2,::]**2
+                                                       + pblock[3,::]**2 ) ) /cv
+    if mhd :
+      pblock[4,::] = pblock[4,::] - 0.5 *( pblock[5,::]**2
+                                          +pblock[6,::]**2
+                                          +pblock[7,::]**2 )  /cv
+    pblock[4,::] = pblock[4,::] * rhosc * vsc**2
+  if (mhd and equation >= 5 and equation <= 7) :
+    Bsc = np.sqrt(4.*np.pi*rhosc*vsc**2)
+    pblock[equation,::] = pblock[equation,::] * Bsc
+  return pblock[equation,::]
+  
 '''
   Returns a 3D array of a single block for the selected equation
   consider that the index in python is that used in fortran -1 
@@ -49,7 +79,8 @@ def readbin3d_block(file_in, equation, verbose=False):
     count=(nx+2*nghost)*(ny+2*nghost)*(nz+2*nghost)*neqs)\
     .reshape(neqs,nx+2*nghost,ny+2*nghost,nz+2*nghost,order='F')
   f.close()
-  return data[equation,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]
+  primit = u2prim(file_in,data[::,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)],equation)
+  return primit
 
 '''
   Returns the 3D array for equation neq in all the domain
@@ -135,7 +166,8 @@ def get_scalings(nout,path='',base='points',verbose=False):
   file_in = path+base+str(0).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
   head_info = read_header(file_in,verbose=False)
   rsc, vsc, rhosc = head_info[9]
-  return (rsc, vsc, rhosc)
+  Bsc = np.sqrt(4.*np.pi*rhosc*vsc**2)
+  return (rsc, vsc, rhosc, Bsc)
 
 
 '''
@@ -211,26 +243,3 @@ def get_2d_cut(cut,pos,nout,neq,path='',base='points',verbose=False):
 
   return  map2d.T
 
-  '''
- 
-       
-        if cut == 1 :
-
-          if ( (pos >= ip*nx) and (pos <= (ip+1)*nx-1)) :
-            file_in = path+base+str(proc).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
-            print file_in
-            head_info = read_header(file_in,verbose=False)
-            f                   = head_info[0]
-            nx, ny, nz          = head_info[2]
-            x0, y0, z0          = head_info[4]
-            f.close()
-            block = readbin3d_block(file_in, neq, verbose=verbose)
-            offset=pos-ip*nx
-            print pos, offset
-            #map2d[y0:y0+ny,z0:z0+nz]= block[offset,::,::]
-
-        proc = proc+1        
-    return map2d.T
-
-
-'''
--- py/test.py
+++ py/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import numpy as np
+import matplotlib.pyplot as plt
 import struct
 from guacho_utils import *
 from matplotlib.colors import LogNorm
@@ -8,22 +9,24 @@ plt.ion()
 rmin=100.
 rmax=1e6
 
-path = "/datos_europa/esquivel/Guacho-Test/BIN/"
-nout = 15
+path = '/datos_diable/esquivel/Guacho-1.2/chemH-M2/BIN/'
+nout = 0
 
-rho = readbin3d_all(nout=nout,neq=0,path=path,verbose=True)
+rho = readbin3d_all(nout=nout,neq=0,path=path,verbose=False)
 plt.figure(1)
 plt.clf()
-plt.imshow(rho[::,50,::], norm=LogNorm(), vmin=rmin, vmax=rmax, origin='lower', cmap='Blues' )
+plt.imshow(rho[::,96,::], norm=LogNorm(), vmin=rmin, vmax=rmax, origin='lower', cmap='Blues' )
 plt.colorbar()
 print '****'
 
-cut= get_2d_cut(2,50,nout=nout,neq=0,path=path,verbose=False)
+cut= get_2d_cut(2,96,nout=nout,neq=0,path=path,verbose=False)
 plt.figure(2)
 plt.clf()
 plt.imshow(cut, norm=LogNorm(), vmin=rmin, vmax=rmax, origin='lower', cmap='Blues' )
 plt.colorbar()
 
+vz = readbin3d_all(nout=nout,neq=3,path=path,verbose=False)
+Pgas = readbin3d_all(nout=nout,neq=4,path=path,verbose=False)
 
 #x_ax, y_ax, z_ax = get_axis(nout=0, path=path,verbose=False)
 
