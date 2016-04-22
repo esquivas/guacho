b1ab1f0909ce11b7a5c5ff72959036075b2de038
Alejandro Esquivel <esquivas@gmail.com>
2016-04-22 12:00:09 -0300

    Change prprocessor directives to named constants

--- chemH/parameters.f90
+++ chemH/parameters.f90
@@ -27,44 +27,124 @@
 !! can be moved later to a runtime input file
 
 module parameters
-  use constants, only : Rg, amh, au, yr
+  use constants
   implicit none
 #ifdef MPIP
   include "mpif.h"
 #endif
+
+  !----------------------------------------
+  !  setup parameters
+  !  If logical use true or false
+  !  If integer, choose from list provided
+  !----------------------------------------
+  logical, parameter :: mpip     = .true.   !<  enable mpi parallelization
+  logical, parameter :: doublep  = .true.   !<  enable double precision
+  logical, parameter :: passives = .true.   !<  enable passive scalars
+  logical, parameter :: pmhd     = .false.  !<  enadble passive mhd
+  logical, parameter :: mhd      = .true.   !<  Enable full MHD
+  
+  !> Approximate Riemman Solver
+  !> SOLVER_HLL  : HLL solver (HD most diffusive)
+  !> SOLVER_HLLC : HLLC solver 
+  !> SOLVER_HLLE : HLLE solver (too diffusive)
+  !> SOLVER_HLLD : HLLD solver
+  integer, parameter :: riemann_solver = SOLVER_HLLC
+
+  !  Type of output
+  logical, parameter :: out_bin  = .true.   !< binary i/o (needed for warmstart)
+  logical, parameter :: out_vtk  = .false.  !< vtk (also binary)
+  logical, parameter :: out_silo = .false.  !< silo (needs hdf and silo libraries)
+  !! Silo also has to be enabled in Makefile
+
+  !> Equation of state used to compute T and P
+  !> EOS_ADIABATIC     : Does not modify P, and T=(P/rho)*Tempsc
+  !> EOS_SINGLE_SPECIE : Uses only n (e.g. to use with tabulated cooling curves)
+  !> H_RATE            : Using n_HI and n_HII
+  !> CHEM              : Enables a full chemical network
+  logical, parameter :: eq_of_state = EOS_CHEM
+
+  !> Type of cooling (choose only one)
+  !> COOL_NONE: Turns off the cooling
+  !> COOL_H    : Single parametrized cooling function (ionization fraction and T)
+  !> COOL_BBC  : Cooling function of Benjamin, Benson and Cox (2003)
+  !> COOL_DMC  : coronal eq. (tabulated) from Dalgarno & Mc Cray (1972)
+  !> COOL_CHI  : From table(s) generated with Chianti
+  !> COOL_CHEM : enables cooling from a full chemical network
+  logical, parameter :: cooling = COOL_NONE
+
+  !> Boundary conditions
+  !> BC_OUTFLOW   :  Outflow boundary conditions (free flow)
+  !> BC_CLOSED    :  Closed BCs, (aka reflective)
+  !> BC_ PERIODIC : Periodic BCs
+  integer, parameter :: bc_left   = BC_OUTFLOW
+  integer, parameter :: bc_right  = BC_OUTFLOW
+  integer, parameter :: bc_bottom = BC_OUTFLOW
+  integer, parameter :: bc_top    = BC_OUTFLOW
+  integer, parameter :: bc_out    = BC_CLOSED
+  integer, parameter :: bc_in     = BC_OUTFLOW
+  logical, parameter :: bc_other  = .true. !< user boundaries (e.g. sources)
+
+  !>  Slope limiters
+  !>  LIMITER_NO_AVERAGE = Performs no average (1st order in space)
+  !>  LIMITER_NO_LIMIT   = Does not limit the slope (unstable)
+  !>  LIMITER_MINMOD     = Minmod (most diffusive) limiter
+  !>  LIMITER_VAN_LEER   = Van Ler limiter
+  !>  LIMITER_VAN_ALBADA = Van Albada limiter
+  !>  LIMITER_UMIST      = UMIST limiter
+  !>  LIMITER_WOODWARD   = Woodward limiter
+  !>  LIMITER_SUPERBEE   = Superbee limiter
+  integer, parameter :: slope_limiter = LIMITER_MINMOD
+
+  !>  Thermal conduction
+  !> TC_OFF         : No thermal conduction
+  !> TC_ISOTROPIC   : Isotropic thermal conduction
+  !> TC_ANISOTROPIC : Anisotropic thermal conduction (requires B field)
+  integer, parameter :: th_cond = TC_OFF
+  logical, parameter :: tc_saturation = .false.   !< Enable Saturation in Thermal Conduction
+
+  !> Enable 'diffuse' radiation
+  logical, parameter :: dif_rad = .false.
+
+  !> Include gravity (Y/N) (from point sources)   
+  logical, parameter :: point_grav = .false.
+
+  !> Include radiative pressure
+  logical, parameter :: radiation_pressure = .false.
+
+  !>   Include terms proportional to DIV B (powell et al. 1999)
+  logical, parameter :: eight_wave = .false.
+  !>  Enable writting of divB to disk
+  logical, parameter :: dump_divb = .false.
+
+  !>  Enable charge exchange
+  logical, parameter :: charge_exchange = .false.
+
+
   !> Path used to write the output
  character (len=128),parameter ::  outputpath='/datos/esquivel/Guacho-1.2/chemcol/'
  !> working directory
  character (len=128),parameter ::  workdir='./'
   
-#if defined(MHD) || defined(PMHD)
-  integer, parameter :: neqdyn=8        !< num. of eqs  (+scal)
-#else
-  integer, parameter :: neqdyn=5        !< num. of eqs  (+scal)
-#endif
-  integer, parameter :: ndim=3          !< num. of dimensions
-#ifdef PASSIVES
-  integer, parameter :: npas=5          !< num. of passive scalars 
-#else
-  integer, parameter :: npas=0          !< num. of passive scalars 
-#endif
-  integer, parameter :: nghost=2        !< num. of ghost cells
+  !> number of dynamical equations, set to 5 for HD and 8 for MHD 
+  ! passibe MHD (PMHD)
+  integer, parameter :: neqdyn=5      !< num. of eqs  (+scal)
+  integer, parameter :: ndim=3        !< num. of dimensions
+  integer, parameter :: npas=5        !< num. of passive scalars (set to 0 if not enabled)
+  integer, parameter :: nghost=2      !< num. of ghost cells
 
-  integer, parameter :: nxtot=192       !< Total grid size in X
-  integer, parameter :: nytot=192       !< Total grid size in Y
-  integer, parameter :: nztot=512       !< Total grid size in Z
+  integer, parameter :: nxtot=192     !< Total grid size in X
+  integer, parameter :: nytot=192     !< Total grid size in Y
+  integer, parameter :: nztot=512     !< Total grid size in Z
 
 #ifdef MPIP
   !   mpi array of processors
-  integer, parameter :: MPI_NBX=2        !< number of MPI blocks in X
-  integer, parameter :: MPI_NBY=2        !< number of MPI blocks in Y
-  integer, parameter :: MPI_NBZ=8        !< number of MPI blocks in Z   
+  integer, parameter :: MPI_NBX=2     !< number of MPI blocks in X
+  integer, parameter :: MPI_NBY=2     !< number of MPI blocks in Y
+  integer, parameter :: MPI_NBZ=8     !< number of MPI blocks in Z   
   !> total number of MPI processes
   integer, parameter :: np=MPI_NBX*MPI_NBY*MPI_NBZ
 #endif
-
-  !   some parameters for coupling with C2-ray
-  !real, parameter :: nphot=5.10e48, ts=1.e5 !Number of Phot. source temp.
 
   !  set box size   
   real, parameter :: xmax=1.         !< grid extent in X (code units)
@@ -82,18 +162,12 @@ module parameters
   real, parameter :: rsc=xphys/xmax         !<  distance scaling
   real, parameter :: rhosc=amh*mu           !<  mass density scaling
   real, parameter :: Tempsc=T0*gamma        !<  Temperature scaling
-  real, parameter :: vsc2 = gamma*Rg*T0/mu  !<  Velocity scaling
-  real, parameter :: vsc = sqrt(vsc2)
+  real, parameter :: vsc2 = gamma*Rg*T0/mu  !<  Velocity scaling squared
+  real, parameter :: vsc = sqrt(vsc2)       !<  Velocity scaling
   real, parameter :: Psc = rhosc*vsc2       !<  Pressure scaling
   real, parameter :: tsc =rsc/sqrt(vsc2)    !<  time scaling
-#ifdef PMHD
-!> magnetic fiewld scaling
-   real, parameter :: bsc = sqrt(4.0*pi*Psc)
-#endif
-#ifdef MHD
-!> magnetic fiewld scaling
-   real, parameter :: bsc = sqrt(4.0*pi*Psc)     
-#endif
+  real, parameter :: bsc = sqrt(4.0*pi*Psc)  !< magnetic field scaling
+
   !> Maximum integration time
   real, parameter :: tmax    = 2.E3*yr/tsc
   !> interval between consecutive outputs
