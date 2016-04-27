bf7d6525496525ed612f84e819cda662193b6086
Alejandro Esquivel <esquivas@gmail.com>
2016-04-27 12:26:52 -0300

    Add order to user boundaries

--- chemH/user_mod.f90
+++ chemH/user_mod.f90
@@ -101,16 +101,22 @@ end subroutine initial_conditions
 !> @param real [out] u(neq,nxmin:nxmax,nymin:nymax,nzmin:nzmax) : 
 !! conserved variables
 !> @param real [in] time : time in the simulation (code units)
+!> @param integer [in] order : order (mum of cells to be filled in case
+!> domain boundaries are being set)
 
-subroutine impose_user_bc(u)
+subroutine impose_user_bc(u,order)
 
   use parameters, only : neq, nxmin, nxmax, nymin, nymax, nzmin, nzmax, bc_other
   use globals   , only : time 
   implicit none
   real :: u(neq,nxmin:nxmax,nymin:nymax,nzmin:nzmax)
+  integer, intent(in) : order
 
-  if (bc_other) call impose_jet(u, time )
- 
+  !  In this case the boundary is the same for 1st and second order)
+  if (order >= 1) then 
+    if (bc_other) call impose_jet(u, time )
+  end if
+
 end subroutine impose_user_bc
 
 !=======================================================================
