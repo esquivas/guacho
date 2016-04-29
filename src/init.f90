efe5c26b06ecb9156fd1e4a58f2d0d619d722a03
Alejandro Esquivel <esquivas@gmail.com>
2016-04-29 09:19:49 -0300

    add support for split

--- src/init.f90
+++ src/init.f90
@@ -315,13 +315,13 @@ subroutine initmain(tprint, itprint)
   end if
   if (bc_bottom == BC_PERIODIC .and. bc_top == BC_PERIODIC) then
     print'(a)', 'BOTTOM & TOP: PERIODIC'
-  else if (bc_bottom == BC_PERIODIC .or. bc_top /= bc_bottom) then
+  else if (bc_bottom == BC_PERIODIC .and. bc_top /= bc_bottom) then
     print'(a)', 'Invalid periodic BCs'
     stop
   end if
   if (bc_out == BC_PERIODIC .and. bc_in == BC_PERIODIC) then
     print'(a)', 'IN & OUT: PERIODIC'
-  else if (bc_out == BC_PERIODIC .or. bc_in /= bc_top) then
+  else if (bc_out == BC_PERIODIC .and. bc_in /= bc_out) then
     print'(a)', 'Invalid periodic BCs'
     stop
   end if
