df4c76af79bb77d385dbdd22f452a842409e148c
Alejandro Esquivel <esquivas@gmail.com>
2017-05-08 10:45:40 -0500

    fix typo

--- CE/cooling_h.f90
+++ CE/cooling_h.f90
@@ -244,7 +244,7 @@ subroutine  cooling_h_neq(pp, uu, dt, radphi)
   !  update pressure
   pp(5) = pp(5) * ch_factor
 
-  !  set pressure flor "a la mala"
+  !  set pressure floor "a la mala"
   pp(5) = max(pp(5),(pp(1)+pp(11))*10000./Tempsc)
 
   !  update total energy density
