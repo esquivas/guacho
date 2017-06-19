1486e274f94522edd4bd2b47d536e84444aefba2
Alejandro Esquivel <esquivas@gmail.com>
2017-06-19 15:57:55 -0500

    fix typo

--- src/lyman_alpha_tau.f90
+++ src/lyman_alpha_tau.f90
@@ -142,7 +142,7 @@ subroutine read_data(u,itprint,filepath)
   integer, intent(in) :: itprint
   character (len=128), intent(in) :: filepath
   character                       :: byte_read
-  integer :: nxp, nyp, nzp, dxp, dyp, dzp. x0p, y0p, z0p, &
+  integer :: nxp, nyp, nzp, dxp, dyp, dzp, x0p, y0p, z0p, &
              mpi_xp, mpi_yp, mpi_zp,neqp, neqdynp, nghostp
   integer :: unitin, ip, err
   character (len=128) file1
