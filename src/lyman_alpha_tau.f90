e6bcf8c88fd3c788daf31deb0a044863b958b21c
Alejandro Esquivel <esquivas@gmail.com>
2018-08-28 20:05:38 -0300

    Change to output w/header compatibility

--- src/lyman_alpha_tau.f90
+++ src/lyman_alpha_tau.f90
@@ -66,13 +66,13 @@ implicit none
   dims(0)  =MPI_NBX
   dims(1)  =MPI_NBY
   dims(2)  =MPI_NBZ
-  
+
   call mpi_init (err)
   call mpi_comm_rank (mpi_comm_world,rank,err)
   call mpi_comm_size (mpi_comm_world,nps,err)
   if (nps.ne.np) then
      print*, 'processor number (',nps,') is not equal to pre-defined number (',np,')'
-     call mpi_finalize(err) 
+     call mpi_finalize(err)
      stop
   endif
 #else
@@ -100,11 +100,11 @@ implicit none
   call mpi_comm_rank(comm3d, rank, err)
   call mpi_cart_coords(comm3d, rank, ndim, coords, err)
   print '(a,i3,a,3i4)', 'processor ', rank                              &
-       ,' ready w/coords',coords(0),coords(1),coords(2)   
+       ,' ready w/coords',coords(0),coords(1),coords(2)
   call mpi_cart_shift(comm3d, 0, 1, left  , right, err)
   call mpi_cart_shift(comm3d, 1, 1, bottom, top  , err)
   call mpi_cart_shift(comm3d, 2, 1, out   , in   , err)
-  call mpi_barrier(mpi_comm_world, err)   
+  call mpi_barrier(mpi_comm_world, err)
   !
 #else
   print '(a)' ,'*******************************************'
@@ -141,9 +141,13 @@ subroutine read_data(u,itprint,filepath)
   real, intent(out) :: u(neq,nxmin:nxmax,nymin:nymax,nzmin:nzmax)
   integer, intent(in) :: itprint
   character (len=128), intent(in) :: filepath
+  character                       :: byte_read
+  integer :: nxp, nyp, nzp, x0p, y0p, z0p, &
+             mpi_xp, mpi_yp, mpi_zp,neqp, neqdynp, nghostp
   integer :: unitin, ip, err
+    real :: dxp, dyp, dzp, scal(3), cvp
   character (len=128) file1
-  
+
   take_turns : do ip=0,np-1
     if (rank == ip) then
 
@@ -156,12 +160,27 @@ subroutine read_data(u,itprint,filepath)
               trim(filepath)//'BIN/points',itprint,'.bin'
          unitin=10
 #endif
-         open(unit=unitin,file=file1,status='unknown', access='stream', &
-              convert='LITTLE_ENDIAN')
-         !
+         open(unit=unitin,file=file1,status='unknown', access='stream')
+
+         !   discard the ascii header
+         do while (byte_read /= achar(255) )
+           read(unitin) byte_read
+           !print*, byte_read
+         end do
+         !  read bin header, sanity check to do
+         read(unitin) byte_read
+         read(unitin) byte_read
+         read(unitin) nxp, nyp, nzp
+         read(unitin) dxp, dyp, dzp
+         read(unitin) x0p, y0p, z0p
+         read(unitin) mpi_xp, mpi_yp, mpi_zp
+         read(unitin) neqp, neqdynp
+         read(unitin) nghostp
+         read(unitin) scal(1:3)
+         read(unitin) cvp
          read(unitin) u(:,:,:,:)
          close(unitin)
-         !
+
          print'(i3,a,a)',rank,' read file:',trim(file1)
 
     end if
@@ -178,9 +197,9 @@ end subroutine read_data
 !> @param integer [in] i : cell index in the x direction
 !> @param integer [in] j : cell index in the y direction
 !> @param integer [in] k : cell index in the z direction
-!> @param real    [in] x : x position in the grid 
-!> @param real    [in] y : y position in the grid 
-!> @param real    [in] z : z position in the grid 
+!> @param real    [in] x : x position in the grid
+!> @param real    [in] y : y position in the grid
+!> @param real    [in] z : z position in the grid
 
   subroutine getXYZ(i,j,k,x,y,z)
 
@@ -189,11 +208,11 @@ end subroutine read_data
     implicit none
     integer, intent(in)  :: i, j, k
     real,    intent(out) :: x, y, z
- 
-    x=(float(i+coords(0)*nx-nxtot/2)+0.5)*dx
-    y=(float(j+coords(1)*ny-nytot/2)+0.5)*dy
-    z=(float(k+coords(2)*nz-nztot/2)+0.5)*dz
-        
+
+    x=(float(i+coords(0)*nx-nxtot/2) - 0.5)*dx
+    y=(float(j+coords(1)*ny-nytot/2) - 0.5)*dy
+    z=(float(k+coords(2)*nz-nztot/2) - 0.5)*dz
+
   end subroutine getXYZ
 
 !=======================================================================
@@ -270,7 +289,7 @@ subroutine rotation_x(theta,x,y,z,xn,yn,zn)
 !> @details Fills the target map of one MPI block
 !> @param integer [in] nxmap : Number of X cells in target
 !> @param integer [in] nymap : Number of Y cells in target
-!> @param real [in] u(neq,nxmin:nxmax,nymin:nymax, nzmin:nzmax) : 
+!> @param real [in] u(neq,nxmin:nxmax,nymin:nymax, nzmin:nzmax) :
 !! conserved variables
 !> @param real [out] map(nxmap,mymap) : Target map
 !> @param real [in] dxT : target pixel width
@@ -292,7 +311,7 @@ subroutine fill_map(nxmap,nymap,nvmap,vmin,vmax,u,map,dxT,dyT,&
   implicit none
 
   integer, intent(in) :: nxmap,nymap,nvmap
-  real, intent(in) :: vmin, vmax 
+  real, intent(in) :: vmin, vmax
   real, intent(in) :: u(neq,nxmin:nxmax,nymin:nymax, nzmin:nzmax)
   real , intent(in) :: dxT, dyT, theta_x, theta_y, theta_z
   real, intent(out) :: map(nxmap,nymap,nvmap)
@@ -309,7 +328,7 @@ subroutine fill_map(nxmap,nymap,nvmap,vmin,vmax,u,map,dxT,dyT,&
           !  obtain original position
           call getXYZ(i,j,k, x,y,z)
           !only do to the side facing the observer
-          if(z < -30.*dz ) then
+          if(z > 30.*dz ) then
             !  do the rotation of the coordinates
             call rotation_x(theta_x,x,y,z,xn,yn,zn)
             call rotation_y(theta_y,xn,yn,zn,x,y,z)
@@ -330,14 +349,14 @@ subroutine fill_map(nxmap,nymap,nvmap,vmin,vmax,u,map,dxT,dyT,&
             call rotation_z(theta_z,vx,vy,vz,vxn,vyn,vzn)
 
             !  calculate the line profile function
-            call phigauss(T, vzn,vmin,vmax,nvmap,profile) 
+            call phigauss(T, vzn,vmin,vmax,nvmap,profile)
             !  make sure the result lies in the map bounds
             if( (iobs >=1    ).and.(jobs >=1    ).and. &
                 (iobs <=nxmap).and.(jobs <=nymap) ) then
-              if ((T < 1e5).and.(prim(7)<0)) then
+              !if ((T < 1e5).and.(prim(7)<0)) then
               map(iobs,jobs,:)= map(iobs,jobs,:) + &
                                   dz*rsc*prim(neqdyn+1)*sigmaLA*lambdaLA*profile(:)
-              end if
+              !end if
             end if
           end if
       end do
@@ -358,7 +377,6 @@ end subroutine fill_map
 !> @param real [in] map(nxmap,mymap) : Target map
 
 subroutine  write_LA(itprint,filepath,nxmap,nymap,nvmap,map)
-
   implicit none
   integer, intent(in) :: nxmap, nymap,nvmap,itprint
   character (len=128), intent(in) :: filepath
@@ -368,13 +386,13 @@ subroutine  write_LA(itprint,filepath,nxmap,nymap,nvmap,map)
 
   write(file1,'(a,i3.3,a)')  trim(filepath)//'BIN/LA_tau-',itprint,'.bin'
   unitout=11
-  open(unit=unitout,file=file1,status='unknown',acess='stream', &
-       convert='LITTLE_ENDIAN')
-  
+  open(unit=unitout,file=file1,status='unknown',access='stream')
+
   write (unitout) map(:,:,:)
   close(unitout)
+
   print'(a,a)'," wrote file:",trim(file1)
-  
+
 end subroutine write_LA
 
 !=======================================================================
@@ -382,7 +400,7 @@ end subroutine write_LA
 !> @brief This routine computes a gaussian line profile
 !> @details This routine computes a gaussian line profile
 
-subroutine phigauss(T,vzn,vmin,vmax,nvmap,profile) 
+subroutine phigauss(T,vzn,vmin,vmax,nvmap,profile)
 
   use constants, only: amh, pi, kB, clight
   implicit none
@@ -391,17 +409,17 @@ subroutine phigauss(T,vzn,vmin,vmax,nvmap,profile)
   real, intent(out) :: profile(nvmap)
   integer :: i
   real :: coef, dv, vr
-  
+
   profile(:)=0.
   dv=(vmax-vmin)/float(nvmap)
-  
+
   coef=amh/(2.*kB*T)
-  
+
   do i=1,nvmap
      vr=(float(i)-0.5)*dv+vmin
      profile(i)=sqrt(coef/pi)*exp(-coef*((vr-vzn)**2) )
   end do
-  
+
 end subroutine phigauss
 
 !=======================================================================
@@ -419,8 +437,9 @@ end module lyman_alpha_utilities
 program lyman_alpha_tau
 
   use constants, only : pi
-  use parameters, only : xmax,master, mpi_real_kind
-  use globals, only : u, dx, dy, rank, comm3d
+  use parameters, only : xmax,master, mpi_real_kind, &
+                         outputpath, nxtot, nytot
+  use globals, only : u, rank, comm3d
   use lyman_alpha_utilities
 
   implicit none
@@ -430,14 +449,21 @@ program lyman_alpha_tau
   character (len=128) :: filepath
   integer :: err
   integer :: itprint
-  ! 
+  !
   real, parameter :: theta_x =-3.33 *pi/180.
   real, parameter :: theta_y = 0.00 *pi/180.
   real, parameter :: theta_z = 0.00 *pi/180.
   !   map and its dimensions
-  integer, parameter :: nxmap=400, nymap=400,nvmap=250
+  integer, parameter :: nvmap=250
+  integer            :: nxmap, nymap
   real :: dxT, dyT, vmin,vmax
-  real :: map(nxmap, nymap,nvmap), map1(nxmap, nymap,nvmap)
+  real, allocatable :: map(:,:,:), map1(:,:,:)
+  !real :: map(nxmap, nymap,nvmap), map1(nxmap, nymap,nvmap)
+
+  nxmap = nxtot
+  nymap = nytot
+  allocate( map(nxmap, nymap,nvmap))
+  allocate(map1(nxmap, nymap,nvmap))
 
   ! initializes program
   call init_LA()
@@ -448,15 +474,15 @@ program lyman_alpha_tau
   !  Target pixel size, relative to the simulation
   dxT= xmax/float(nxmap)
   dyT= dxT
-  
-  ! chose output (fix later to input form screen)
-  filepath='/datos/esquivel/EXO-GUACHO/P1c/'
 
-  loop_over_outputs : do itprint=68,75
-    
+  ! chose output (fix later to input form screen)
+  filepath=trim(outputpath) !'/datos/esquivel/EXO-GUACHO/P1c/'
+
+  loop_over_outputs : do itprint=0,70
+
     !  read ph and u from file
     call read_data(u,itprint,filepath)
-    
+
     !  resets map
     map(:,:,:)=0.
     map1(:,:,:)=0.
@@ -467,17 +493,17 @@ program lyman_alpha_tau
                                     ,theta_y*180./pi,'° around Y, '&
                                     ,theta_z*180./pi,'° around Z, '
     end if
-    
+
     !  add info to the map
     call fill_map(nxmap,nymap,nvmap,vmin,vmax,u,map,dxT,dyT, theta_x, theta_y, theta_z)
     !  sum all the partial sums
     call mpi_reduce(map,map1,nxmap*nymap*nvmap, mpi_real_kind, mpi_sum, master, comm3d, err)
-    
+
     !  write result
-    if (rank == master) then 
+    if (rank == master) then
       call write_LA(itprint,filepath,nxmap,nymap,nvmap,map1)
     end if
-    
+
   end do loop_over_outputs
 
   if (rank == master) print*, 'my work here is done, have a  nice day'
@@ -490,8 +516,3 @@ program lyman_alpha_tau
 end program lyman_alpha_tau
 
 !=======================================================================
-
-
-
-
-
