9e6a5862b425845bcef896b7320a28b90cbfb9c8
Alejandro Esquivel <esquivas@gmail.com>
2019-02-13 10:48:10 -0300

    Get temperature, beta version

--- py/guacho_utils.py
+++ py/guacho_utils.py
@@ -91,6 +91,36 @@ def readbin3d_block(file_in, equation, verbose=False, mhd = False, entropy=False
   return primit
 
 '''
+   Returns a 3D array with the Temperature
+   ***** Please make sure this coincides with the EOS used***
+   This imnplementation assumes that an EOS_H is used and that MHD is off
+'''
+def get_3D_temperature(file_in, equation, verbose=False):
+    head_info = read_header(file_in,verbose=verbose)
+    f                   = head_info[0]
+    f_kind              = head_info[1]
+    nx, ny, nz          = head_info[2]
+    neqs                = head_info[6]
+    nghost              = head_info[8]
+    rsc, vsc, rhosc     = head_info[9]
+    cv                  = head_info[10]
+    data = np.fromfile(f, dtype=f_kind, \
+      count=(nx+2*nghost)*(ny+2*nghost)*(nz+2*nghost)*neqs)\
+      .reshape(neqs,nx+2*nghost,ny+2*nghost,nz+2*nghost,order='F')
+    f.close()
+    #  The following should be consistent with the u2prim in hydro_core.f90
+    #  Scaling in the follwing two lines has been omitted by assuming that rhosc = mh
+    n_tot = data[0,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)] # *rhosc/mh
+    n_h0  = data[5,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)] # *rhosc/mh
+    Pgas  = ( data[4,0,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]
+            - ( data[1,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]**2
+              + data[2,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]**2
+              + data[3,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]**2 )/n_tot ) * cv
+    Tsc = vsc**2/8.3145e7   #
+    Temp = P/(2.*ntot-nh0) * Tsc
+  return Temp
+
+'''
   Returns the 3D array for equation neq in all the domain
 '''
 def readbin3d_all(nout,neq,path='',base='points',verbose=False, mhd=False, entropy=False, scale=True, conserved = False):
@@ -115,7 +145,10 @@ def readbin3d_all(nout,neq,path='',base='points',verbose=False, mhd=False, entro
         if proc ==0 :
           map3d=np.zeros(shape=(nx*mpi_x,ny*mpi_y,nz*mpi_z))
         proc = proc+1
-        map3d[x0:x0+nx,y0:y0+ny,z0:z0+nz] = readbin3d_block(file_in, neq, verbose=verbose, mhd= mhd, entropy = entropy, scale=scale, conserved=conserved)
+        if (neq == -1) :
+            map3d[x0:x0+nx,y0:y0+ny,z0:z0+nz] =get_3D_temperature(file_in, verbose=verbose)
+        else :
+            map3d[x0:x0+nx,y0:y0+ny,z0:z0+nz] = readbin3d_block(file_in, neq, verbose=verbose, mhd= mhd, entropy = entropy, scale=scale, conserved=conserved)
   return map3d.T
 
 '''
