4b8251093d2e4908d37f4d1cf6c67fd6729fe23f
Alejandro Esquivel <esquivas@gmail.com>
2019-02-14 16:06:01 -0300

    guacho utilities for python 3x

--- py/guacho_utils.py
+++ py/guacho_utils.py
@@ -9,25 +9,25 @@ def read_header(file_in, verbose=True):
   f = open(file_in,'rb')
   while 1==1 :
       s = f.readline().rstrip()
-      if s == '\xff' :
+      if s == b'\xff' :
         break
       if (verbose) :
-        print s
+        print (s)
   f_kind = struct.unpack('s',f.read(1))[0]
   nx, ny, nz = struct.unpack('3i',f.read(12))
-  if (f_kind == 'd'):
+  if (f_kind == b'd'):
     dx, dy, dz = struct.unpack('3d',f.read(24))
-  if (f_kind == 'f'):
+  if (f_kind == b'f'):
     dx, dy, dz = struct.unpack('3f',f.read(12))
   x0, y0, z0 = struct.unpack('3i',f.read(12))
   mpi_x, mpi_y, mpi_z = struct.unpack('3i',f.read(12))
   neqs   = struct.unpack('1i',f.read(4))[0]
   neqdyn = struct.unpack('1i',f.read(4))[0]
   nghost = struct.unpack('1i',f.read(4))[0]
-  if (f_kind == 'd'):
+  if (f_kind == b'd'):
     rsc, vsc, rhosc = struct.unpack('3d',f.read(24))
     cv              = struct.unpack( 'd',f.read(8))
-  if (f_kind == 'f'):
+  if (f_kind == b'f'):
     rsc, vsc, rhosc = struct.unpack('3f',f.read(12))
     cv              = struct.unpack( 'f',f.read(4))
   return (f,f_kind,(nx,ny,nz),(dx,dy,dz),(x0,y0,z0),\
@@ -36,7 +36,7 @@ def read_header(file_in, verbose=True):
   '''
     converts conserved variables to primitives (and de-scales to cgs)
   '''
-def u2prim(file_in, ublock, equation, mhd = False, entropy=False, scale=True) :
+def u2prim(file_in, ublock, equation, mhd = False, entropy=False, scale=True, conserved=False) :
   head_info = read_header(file_in,verbose=False)
   if scale:
       rsc, vsc, rhosc = head_info[9]
@@ -87,45 +87,15 @@ def readbin3d_block(file_in, equation, verbose=False, mhd = False, entropy=False
   if (conserved):
     primit = data[equation,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]
   else :
-    primit = u2prim(file_in,data[::,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)],equation, mhd = mhd, entropy = entropy, scale=scale)
+    primit = u2prim(file_in,data[::,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)],equation, mhd = mhd, entropy = entropy, scale=scale, conserved=conserved)
   return primit
-
-'''
-   Returns a 3D array with the Temperature
-   ***** Please make sure this coincides with the EOS used***
-   This imnplementation assumes that an EOS_H is used and that MHD is off
-'''
-def get_3D_temperature(file_in, equation, verbose=False):
-    head_info = read_header(file_in,verbose=verbose)
-    f                   = head_info[0]
-    f_kind              = head_info[1]
-    nx, ny, nz          = head_info[2]
-    neqs                = head_info[6]
-    nghost              = head_info[8]
-    rsc, vsc, rhosc     = head_info[9]
-    cv                  = head_info[10]
-    data = np.fromfile(f, dtype=f_kind, \
-      count=(nx+2*nghost)*(ny+2*nghost)*(nz+2*nghost)*neqs)\
-      .reshape(neqs,nx+2*nghost,ny+2*nghost,nz+2*nghost,order='F')
-    f.close()
-    #  The following should be consistent with the u2prim in hydro_core.f90
-    #  Scaling in the follwing two lines has been omitted by assuming that rhosc = mh
-    n_tot = data[0,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)] # *rhosc/mh
-    n_h0  = data[5,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)] # *rhosc/mh
-    Pgas  = ( data[4,0,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]
-            - ( data[1,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]**2
-              + data[2,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]**2
-              + data[3,nghost:(nx+nghost),nghost:(ny+nghost),nghost:(nz+nghost)]**2 )/n_tot ) * cv
-    Tsc = vsc**2/8.3145e7   #
-    Temp = P/(2.*ntot-nh0) * Tsc
-  return Temp
 
 '''
   Returns the 3D array for equation neq in all the domain
 '''
 def readbin3d_all(nout,neq,path='',base='points',verbose=False, mhd=False, entropy=False, scale=True, conserved = False):
 
-  print 'Retrieving 3D map for eqn',neq
+  print ('Retrieving 3D map for eqn',neq)
   file_in = path+base+str(0).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
   head_info = read_header(file_in,verbose=False)
   f                   = head_info[0]
@@ -136,7 +106,7 @@ def readbin3d_all(nout,neq,path='',base='points',verbose=False, mhd=False, entro
     for jp in range(mpi_y) :
       for kp in range(mpi_z) :
         file_in = path+base+str(proc).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
-        print file_in
+        print (file_in)
         head_info = read_header(file_in,verbose=False)
         f                   = head_info[0]
         nx, ny, nz          = head_info[2]
@@ -145,10 +115,7 @@ def readbin3d_all(nout,neq,path='',base='points',verbose=False, mhd=False, entro
         if proc ==0 :
           map3d=np.zeros(shape=(nx*mpi_x,ny*mpi_y,nz*mpi_z))
         proc = proc+1
-        if (neq == -1) :
-            map3d[x0:x0+nx,y0:y0+ny,z0:z0+nz] =get_3D_temperature(file_in, verbose=verbose)
-        else :
-            map3d[x0:x0+nx,y0:y0+ny,z0:z0+nz] = readbin3d_block(file_in, neq, verbose=verbose, mhd= mhd, entropy = entropy, scale=scale, conserved=conserved)
+        map3d[x0:x0+nx,y0:y0+ny,z0:z0+nz] = readbin3d_block(file_in, neq, verbose=verbose, mhd= mhd, entropy = entropy, scale=scale, conserved=conserved)
   return map3d.T
 
 '''
@@ -229,13 +196,13 @@ def get_2d_cut(cut,pos,nout,neq,path='',base='points',verbose=False,mhd=False, e
   nztot = nz * mpi_z
   if cut == 1 :
       map2d=np.zeros(shape=(nytot,nztot))
-      print 'YZ cut, equation: ', neq
+      print ('YZ cut, equation: ', neq)
   elif cut == 2 :
       map2d=np.zeros(shape=(nxtot,nztot))
-      print 'XZ cut, equation: ', neq
+      print ('XZ cut, equation: ', neq)
   elif cut == 3 :
       map2d=np.zeros(shape=(nxtot,nytot))
-      print 'XY cut, equation: ', neq
+      print ('XY cut, equation: ', neq)
   proc=0
   for ip in range(mpi_x) :
     for jp in range(mpi_y) :
@@ -244,7 +211,7 @@ def get_2d_cut(cut,pos,nout,neq,path='',base='points',verbose=False,mhd=False, e
         if cut == 1 :
           if ( (pos >= ip*nx) and (pos <= (ip+1)*nx-1)) :
             file_in = path+base+str(proc).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
-            print file_in
+            print (file_in)
             head_info = read_header(file_in,verbose=False)
             f                   = head_info[0]
             nx, ny, nz          = head_info[2]
@@ -257,7 +224,7 @@ def get_2d_cut(cut,pos,nout,neq,path='',base='points',verbose=False,mhd=False, e
         elif cut == 2:
           if ( (pos >= jp*ny) and (pos <= (jp+1)*ny-1)) :
             file_in = path+base+str(proc).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
-            print file_in
+            print (file_in)
             head_info = read_header(file_in,verbose=False)
             f                   = head_info[0]
             nx, ny, nz          = head_info[2]
@@ -270,7 +237,7 @@ def get_2d_cut(cut,pos,nout,neq,path='',base='points',verbose=False,mhd=False, e
         elif cut == 3:
           if ( (pos >= kp*nz) and (pos <= (kp+1)*nz-1)) :
             file_in = path+base+str(proc).zfill(3)+'.'+str(nout).zfill(3)+'.bin'
-            print file_in
+            print (file_in)
             head_info = read_header(file_in,verbose=False)
             f                   = head_info[0]
             nx, ny, nz          = head_info[2]
