53bbe650d9bbbe42443fb965b1887c5f3d99ddde
matias schneiter <mschneiter@gmail.com>
2019-06-25 14:22:51 -0300

    vortex setup

--- py/vortex.py
+++ py/vortex.py
@@ -10,7 +10,7 @@ rmin=.5
 rmax=1.
 
 path = '../pic/BIN/'
-nout = 0
+nout = 33
 
 nproc = get_Nproc(nout,path=path)
 
@@ -28,13 +28,12 @@ plt.colorbar()
 Q= plt.quiver(X[::4],Y[::4],vx[::4],vy[::4], pivot='mid',scale=20)
 
 
-
 part = np.zeros(shape=(50,1024,2) )
 for (nout) in range(0,50):
     id,xp, yp, zp, vxp, vyp, vzp = readpic(nout,path=path)
     part[nout,:,0] = xp[:]
     part[nout,:,1] = yp[:]
-    #plt.plot(xp-5.,yp-5.,'o',markersize= 2)
+    plt.plot(xp-5.,yp-5.,'o',markersize= 2)
 
 for ip in range(1024):
     plt.plot(part[:,ip,0]-5,part[:,ip,1]-5,'-o',markersize= 2)
