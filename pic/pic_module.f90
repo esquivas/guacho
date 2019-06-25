1d534e52b291eaaf4bf1671cd0af0218ab757010
matias schneiter <mschneiter@gmail.com>
2019-06-25 14:22:12 -0300

    exchange particles w/MPI

--- pic/pic_module.f90
+++ pic/pic_module.f90
@@ -19,6 +19,40 @@ contains
   end subroutine init_pic
 
   !================================================================
+  subroutine deactivateMP(i_mp)
+    use globals,    only : partID, n_activeMP
+    implicit none
+    integer, intent(in) :: i_mp
+
+    !  deactivate particle
+    partID(i_mp) = 0
+
+    !  if necessary move tail index
+    if (i_mp == n_activeMP) n_activeMP = n_activeMP - 1
+
+  end subroutine deactivateMP
+
+  !================================================================
+  subroutine addMP(ID, ndata, Qdata, i_mp)
+    use parameters, only : N_MP
+    use globals,    only : partID,Q_MP0
+    implicit none
+    integer, intent(in)  :: ID,ndata
+    real,    intent(in)  :: Qdata(ndata)
+    integer, intent(out) :: i_mp
+
+    ! sweep list and add element in empty slot
+    do i_mp=1, n_mp
+      if(partID(i_mp)==0) then
+        partID(i_mp)  = ID
+        Q_MP0(i_mp,1:ndata) = Qdata(1:ndata)
+        if (i_mp > n_activeMP) n_activeMP = i_mp
+        return
+      end if
+    end do
+
+  end subroutine addMP
+  !================================================================
   subroutine PICpredictor
 
     use globals,   only : primit, dt_CFL, rank, dx, dy, dz,comm3d
@@ -73,7 +107,7 @@ contains
           sendLoc(dest) = sendLoc(dest) + 1
           nLocSend      = nLocSend      + 1
           dataLoc(nLocSend) = i_mp
-          print'(i2,a,i4,a,i4)', rank, ' *** particle ',partID(i_mp), ' is going to ',DEST
+          !print'(i2,a,i4,a,i4)', rank, ' *** particle ',partID(i_mp), ' is going to ',DEST
         end if
 
       end if
@@ -84,27 +118,35 @@ contains
                        sendList, np, mpi_integer, comm3d,err)
 
     !  exchange particles
-    do iS=0,np-1
-      do iR=0,np-1
+    do iR=0,np-1
+      do iS=0,np-1
 
         if(sendList(iR,iS) /= 0) then
           if(iS == rank) then
-            print'(i0,a,i0,a,i0)', rank,'-->', IR, ':',sendList(iR,iS)
-            do i=1,nLocSend
-              print*,'>>>',rank,partID(dataLoc(i)),dataLoc(i)
+            !print'(i0,a,i0,a,i0)', rank,'-->', IR, ':',sendList(iR,iS)
+            do i=1,sendlist(iR,iS)
+              !print*,'>>>',rank,partID(dataLoc(i)),dataLoc(i)
               call mpi_send( Q_MP0(dataLoc(i),1:6) , 6, mpi_real_kind ,IR, &
-                            partID(dataLoc(i)), comm3d,status,err)
+                            partID(dataLoc(i)), comm3d,err)
+              !  deactivate particle from current processor
+              call deactivateMP(dataLoc(i))
             end do
 
           endif
           if(iR == rank) then
-            print'(i0,a,i0,a,i0)', rank,'<--', IS, ':',sendList(iR,iS)
+            !print'(i0,a,i0,a,i0)', rank,'<--', IS, ':',sendList(iR,iS)
             do i=1,sendList(iR,iS)
-              print*,'<<<',rank,' will recv here from ',IS
+              !print*,'<<<',rank,' will recv here from ',IS
 
               call mpi_recv(SingleRec(1:6), 6, mpi_real_kind, IS, mpi_any_tag, &
                             comm3d, status, err)
-              print*,'received successfuly', status(MPI_TAG)
+              !print*,'received successfuly', status(MPI_TAG)
+
+              !  add current particle in list and data in new processor
+              call addMP( status(MPI_TAG), 6, SingleRec(1:6), i_mp )
+
+              !  recalculate predictor step for newcomer
+              Q_MP1(i_mp,1:3) = Q_MP0(i_mp,1:3)+ dt_CFL*Q_MP0(i_mp,4:6)
 
             end do
           end if
@@ -117,51 +159,89 @@ contains
   !================================================================
   subroutine PICcorrector
 
-    use globals,   only : primit, dt_CFL, rank
-    use utilities, only : isInDomain
+    use globals,   only : primit, dt_CFL, rank, comm3d
+    use parameters
+    use utilities, only : inWhichDomain
     implicit none
     integer :: i_mp, i, j, k, l, ind(3)
-    real    :: weights(8), vel1(3)
+    real    :: weights(8), vel1(3), SingleRec(3)
+    integer :: dest, nLocSend, dataLoc(N_mp),  sendLoc(0:np-1), &
+               sendList(0:np-1,0:np-1)
+    integer :: status(MPI_STATUS_SIZE), err, iR, iS
+
+    ! initialize send and recv lists
+    dataLoc(:)    =  0
+    sendLoc(:)    =  0
+    sendlist(:,:) =  0
+    nLocSend      =  0
 
 
     do i_mp=1, n_MP
       ! execute only if particle i_mp is in the active list
       if (partID(i_mp)/=0) then
-
-        ! proceed further only if paricle is in domain
-        if ( isInDomain( Q_MP1(i_mp,1:3) ) ) then
-
-          ! Calculate interpolation reference and weights
-          call interpBD(Q_MP1(i_mp,1:3),ind,weights)
-          !  Interpolate the velocity field to particle position, and add
-          !  to the velocity from the corrector step
-          l=1
-          vel1(:) = 0
-          do k= ind(3),ind(3)+1
-            do j=ind(2),ind(2)+1
-              do i=ind(1),ind(1)+1
-                vel1(1) = vel1(1) + primit(2,i,j,k)*weights(l)
-                vel1(2) = vel1(2) + primit(3,i,j,k)*weights(l)
-                vel1(3) = vel1(3) + primit(4,i,j,k)*weights(l)
-                l = l + 1
-              end do
+        ! Calculate interpolation reference and weights
+        call interpBD(Q_MP1(i_mp,1:3),ind,weights)
+        !  Interpolate the velocity field to particle position, and add
+        !  to the velocity from the corrector step
+        l=1
+        vel1(:) = 0
+        do k= ind(3),ind(3)+1
+          do j=ind(2),ind(2)+1
+            do i=ind(1),ind(1)+1
+              vel1(1) = vel1(1) + primit(2,i,j,k)*weights(l)
+              vel1(2) = vel1(2) + primit(3,i,j,k)*weights(l)
+              vel1(3) = vel1(3) + primit(4,i,j,k)*weights(l)
+              l = l + 1
             end do
           end do
+        end do
 
-          !  corrector step
-          Q_MP0(i_mp,1:3) = Q_MP0(i_mp,1:3) &
-                          + 0.5*dt_CFL*( Q_MP0(i_mp,4:6) + vel1(1:3) )
+        !  corrector step
+        Q_MP0(i_mp,1:3) = Q_MP0(i_mp,1:3) &
+        + 0.5*dt_CFL*( Q_MP0(i_mp,4:6) + vel1(1:3) )
 
-        else
-
-          !  particle has left domain, deactivate it
-          !print'(a,i3,a,i4,a)','---', rank,' particle ', partID(i_mp), ' is no longer with us corr'
-          partID(i_mp) = 0
-          n_activeMP   = n_activeMP - 1
-
+        !  check if particle is leaving the domain
+        dest = inWhichDomain( Q_MP0(i_mp,1:3) )
+        if (dest /= rank) then
+          !  count for MPI exchange
+          sendLoc(dest) = sendLoc(dest) + 1
+          nLocSend      = nLocSend      + 1
+          dataLoc(nLocSend) = i_mp
         end if
 
       end if
+    end do
+
+      !   consolidate list to have info of all send/receive operations
+    call mpi_allgather(sendLoc(:),  np, mpi_integer, &
+                       sendList, np, mpi_integer, comm3d,err)
+
+
+    !  exchange particles
+    do iR=0,np-1
+      do iS=0,np-1
+
+        if(sendList(iR,iS) /= 0) then
+          if(iS == rank) then
+            do i=1,sendlist(iR,iS)
+              call mpi_send( Q_MP0(dataLoc(i),1:3) , 3, mpi_real_kind ,IR, &
+                            partID(dataLoc(i)), comm3d,err)
+              !  deactivate particle from current processor
+              call deactivateMP(dataLoc(i))
+            end do
+
+          endif
+          if(iR == rank) then
+            do i=1,sendList(iR,iS)
+              call mpi_recv(SingleRec(1:3), 3, mpi_real_kind, IS, mpi_any_tag, &
+                            comm3d, status, err)
+              !  add current particle in list and data in new processor
+              call addMP( status(MPI_TAG), 3, SingleRec(1:3), i_mp )
+
+            end do
+          end if
+        end if
+      end do
     end do
 
   end subroutine PICcorrector
@@ -227,7 +307,7 @@ contains
     implicit none
     integer, intent(in) :: itprint
     character(len = 128) :: fileout
-    integer              :: unitout, i_mp
+    integer              :: unitout, i_mp, i_active
 
 #ifdef MPIP
     write(fileout,'(a,i3.3,a,i3.3,a)')  &
@@ -240,8 +320,14 @@ contains
 
   open(unit=unitout,file=fileout,status='unknown',access='stream')
 
+  i_active = 0
+  do i_mp=1,N_MP
+    if(partID(i_mp)/=0) i_active = i_active + 1
+  end do
+
   !  write how many particles are in rank domain
-  write(unitout) np, N_MP, n_activeMP
+  write(unitout) np, N_MP, i_active !n_activeMP
+
 
   !  loop over particles owned by processor and write the active ones
   do i_mp=1,N_MP
