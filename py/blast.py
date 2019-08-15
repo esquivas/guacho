38108040312e4a83051e512f4ecbb6bd46eb2195
Alex Esquivel <esquivas@gmail.com>
2019-08-15 17:34:53 -0300

    work in progress

--- py/blast.py
+++ py/blast.py
@@ -24,7 +24,7 @@ extent =  [0,1,0,1]
 plt.figure(2)
 plt.clf()
 plt.imshow(shockF, extent=extent, origin='lower', cmap='viridis',interpolation = 'none' )
-plt.colorbar()
+#plt.colorbar()
 plt.title('Shock detector (1 if shocked)')
 
 X,Y = np.meshgrid( np.linspace(0.,1.,num=rho.shape[0]),np.linspace(0.,1.,num=rho.shape[1]) )
@@ -39,23 +39,28 @@ plt.figure(3) ; plt.clf()
 
 npart = 250
 for (noutp) in range(nout+1):
-    picData, SED= readpic(noutp,path=path)
+    picData, SED, P1, P2= readpic(noutp,path=path)
+    #picData= readpic(noutp,path=path)
     print(picData.shape)
     plt.figure(1)
     plt.plot(picData[:,1],picData[:,2],'o',markersize= 1)
     plt.plot(picData[npart,1],picData[npart,2],'*',markersize=15, color='green')
-    plt.figure(3)
-    plt.loglog(SED[npart,:,0],SED[npart,:,1]*SED[npart,:,0]**2,label=str(noutp))
+    #plt.figure(3)
+    #plt.loglog(SED[npart,:,0],SED[npart,:,1]*SED[npart,:,0]**2,label=str(noutp))
 
-plt.legend()
-
+#plt.legend()
 plt.figure(2)
-plt.plot(picData[:,1],picData[:,2],'o',markersize=2, color="red",alpha=0.5)
-
-picDataS = readpic(nout,path=path, base='pic-shocked-')
-
-fig = plt.figure(2)
-plt.plot(picDataS[:,1], picDataS[:,2], '*', markersize=5, color='blue',alpha=0.5)
-
-#Q= plt.quiver(X[::8],Y[::8],bx[::8],by[::8], pivot='mid', scale=40, color='w', alpha=0.5)
+sc = plt.scatter(picData[:,1],picData[:,2], c=picData[:,4],alpha=0.5,cmap='inferno',vmin=1.)
+cb = plt.colorbar(sc)
 Q= plt.streamplot( X,Y,bx,by, density = 2., color='silver',linewidth=1., minlength=0.8)
+
+#picDataS = readpic(nout,path=path, base='pic-shocked-')
+#fig = plt.figure(2)
+#plt.plot(picDataS[:,1], picDataS[:,2], '*', markersize=5, color='blue',alpha=0.5)
+#Q= plt.quiver(X[::8],Y[::8],bx[::8],by[::8], pivot='mid', scale=40, color='w', alpha=0.5)
+
+plt.figure(4)
+plt.clf()
+plt.imshow(shockF, extent=extent, origin='lower', cmap='viridis',interpolation = 'none' )
+sc = plt.scatter(picData[:,1],picData[:,2], c=picData[:,5],alpha=0.5,cmap='Spectral')
+cb = plt.colorbar(sc)
