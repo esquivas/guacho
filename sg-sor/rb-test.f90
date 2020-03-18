19860890a56247c2801a6754c783765773fad792
Alex Esquivel <esquivas@gmail.com>
2020-03-18 15:20:06 -0600

    testing checkerboard patterns

--- sg-sor/rb-test.f90
+++ sg-sor/rb-test.f90
@@ -1,38 +1,63 @@
 program test
 	implicit none
-	integer :: i,j,k, ipass, isw,jsw,ksw, nx, ny, nz,count,jpass, kpass
-	nx=4
-	ny=4
-  nz=4
+	integer :: i,j,k, ipass, isw,jsw,ksw,count,jpass, kpass,i_rb
+	integer, parameter :: nx=5, ny=5, nz=5
+	integer :: map(nx,ny,nz)
 
-  count=0
+	map(:,:,:) = 0
+	ksw=1
+	black_red: do kpass=1,2
 
-  print*,'total',count
+		do ipass=1,2
+			jsw=ksw
+			do k=1,nz
+				do j=jsw,ny,2
+					do i=ipass,nx,2
 
-  isw=1
-  do ipass=1,2
-    jsw=isw
-      do i=2, nx-1
-        do j=jsw+1,ny-1,2
+						map(i,j,k) = map(i,j,k) + kpass
 
-          !ksw=isw
-          !do k=ksw+1,nz-1,2
-                
-            print'(4i4)',i,j,k, ipass
-            count=count+1
-          !end do
-              
-          !ksw=3-ksw
-         end do
-         
-        jsw=3-jsw
-        end do
-     
-        print*,'++++++'
-      isw=3-isw
-    !end do
-  end do
+					end do
+				end do
+				jsw=3-jsw
+			end do
+			ksw=3-ksw
+		end do
+	end do black_red
 
-print*,count
+
+	PRINT*,'=============================='
+	do k=1,nz
+		do j=ny,1,-1
+			print'(5i3)', map(:,j,k)
+		end do
+		print*,'-------'
+	end do
+	PRINT*,'=============================='
+
+	map(:,:,:) = 0
+	do i_rb =1,2
+		jsw = i_rb
+		do k=1,nz
+			isw =jsw
+			do j=1,ny
+				do i=isw,nx,2
+
+					map(i,j,k) = map(i,j,k) + i_rb
+
+				end do
+				isw = 3-isw
+			end do
+			jsw = 3-jsw
+		end do
+	end do
+
+	PRINT*,'=============================='
+	do k=1,nz
+		do j=ny,1,-1
+			print'(5i3)', map(:,j,k)
+		end do
+		print*,'-------'
+	end do
+	PRINT*,'=============================='
 
 end program test
