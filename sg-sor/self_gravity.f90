e9af8da7ce74f97482b5f693a73e05def43787e3
Alejandro Esquivel <esquivas@gmail.com>
2020-03-19 18:40:40 -0600

    testing 1, 2, 3 ...

--- sg-sor/self_gravity.f90
+++ sg-sor/self_gravity.f90
@@ -218,12 +218,21 @@ contains
     real, allocatable  :: phiP(:,:,:)
 
     converged = .false.
-    omega=1.5
+    omega=0.99
     e_ijk = -2.0*( 1.0/dx**2 +1.0/dy**2 + 1.0/dz**2 )
 
     if(.not.enable_chebyshev_accel) allocate( phiP(0:nx+1,0:ny+1,0:nz+1) )
 
     main_loop : do iter=1, max_iterations
+
+
+      if (iter < 1000) then
+        omega = 0.99
+      else if (iter < 5000)
+        omega = 1.5
+      else
+        omega = 1.99
+      endif
 
       max_error_local = -1.0
       max_error       = 1e20
@@ -245,7 +254,10 @@ contains
               ph0             = phi_grav(i,j,k)
               phi_grav(i,j,k) = ph0 - omega*xi/e_ijk
 
-              relative_error  = abs(phi_grav(i,j,k)-ph0)/abs(ph0+1e-30)
+              !relative_error  = abs(phi_grav(i,j,k)-ph0)/abs(ph0+1e-30)
+              relative_error  = abs( omega*xi/e_ijk ) /                        &
+                              ( abs(phi_grav(i,j,k)) + 1e-30 )
+
               max_error_local = max(max_error_local,relative_error)
 
             end do
