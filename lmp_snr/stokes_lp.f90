6486046e2ad3695a905e67a00e46cce5d20bff5e
Alejandro Esquivel <esquivas@gmail.com>
2020-04-14 12:39:45 -0500

    debuging

--- lmp_snr/stokes_lp.f90
+++ lmp_snr/stokes_lp.f90
@@ -668,7 +668,7 @@ program stokes_lp
 
   freq_obs  =  1.40e9 !< frequency of observation (Hz)
 
-  loop_over_outputs : do itprint=0,10
+  loop_over_outputs : do itprint=10,10
 
     !  read MHD and particles data
     call read_data(u,itprint,filepath)
