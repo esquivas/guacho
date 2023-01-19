358368524704422f9154ed56ceb61e1e39cba5ee
Alejandro Esquivel <esquivas@gmail.com>
2023-01-19 15:51:42 -0600

    fix bug occurring if iwarm==0 & time_0 /= 0

--- src/init.f90
+++ src/init.f90
@@ -122,24 +122,24 @@ contains
 
     !   initialize time integration
     currentIteration = 1
+    time   = time_0
+    tprint = time_0 + dtprint
+
     if(rank == master) then print'(a)',' '
+    
     if (.not.iwarm) then
       if(rank == master) then
         print'(a,es12.3,a)', 'Starting cold, from t=', time*tsc/yr,' yr'
         print'(a)',' '
       endif
-      itprint=0
-      time= time_0
-      tprint=dtprint
+      itprint = 0
     else
       itprint=itprint0
-      time=time_0
       if(rank == master) then
         print'(a,i0,a,es12.3,a)', 'Warm start , from output ',itprint,         &
                                   ' at a time ',time*tsc/yr,' yr'
         print'(a)',' '
       end if
-      tprint=time+dtprint
     end if
 
     !   allocate big arrays in memory
